0caca17420616734e9f3de6e32ce3c7b090db603
Yichuan <1758783038@qq.com>
2024-05-13 22:15:02 -0500

    copied from old MUTargetCLEAN

--- model.py
+++ model.py
@@ -406,7 +406,7 @@ class Encoder(nn.Module):
         #motif_logits_max, _ = torch.max(motif_logits, dim=-1, keepdim=True).squeeze(-1) #should be [batch,num_class]
         #print(motif_logits_max)
         motif_pro_list=[]
-        print('id_frags_list', len(id_frags_list))
+        print('id_frags_list', id_frags_list)
         for id_protein in id:
             ind_frag=0
             id_frag = id_protein+"@"+str(ind_frag)
