a0301d875210b4a22863ea9c08c924c712d32290
Yichuan <1758783038@qq.com>
2024-05-20 00:11:41 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -85,6 +85,8 @@ class LocalizationDataset(Dataset):
                 """
                 target_list = [[int(max(set(column))) for column in zip(*target)][:len(sequence)] for sequence, target
                                in zip(seq_frag_list, target_frag_list)]
+                print(type(target_list[0]))
+                exit(0)
                 # better_target_list = []
                 # for targets, ptype in zip(target_list, class_positions):
                 #     # print(ptype)
