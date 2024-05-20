e126ee98caac43f81dcd3bd3284e2a7765ae8759
Yichuan <1758783038@qq.com>
2024-05-20 00:04:46 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -85,6 +85,7 @@ class LocalizationDataset(Dataset):
                 """
                 target_list = [[int(max(set(column))) for column in zip(*target)][:len(sequence)] for sequence, target
                                in zip(seq_frag_list, target_frag_list)]
+                better_target_list = []
                 for targets, ptype in zip(target_list, class_positions):
                     # print(ptype)
                     if ptype == 1:
@@ -97,13 +98,25 @@ class LocalizationDataset(Dataset):
                     elif ptype == 0 or ptype == 4:
                         pass
                     elif ptype == 2:
-                        print(targets)
-                        exit(0)
-
+                        idx = targets.index(1)
+                        if idx < len(targets) / 2:
+                            targets[:idx] = [1] * idx
+                        else:
+                            targets[idx + 1:] = [1] * (len(targets) - idx - 1)
+                    else:
+                        for i in range(len(targets)):
+                            if targets[i] == 1:
+                                break
+                            targets[i] = 1
+                    better_target_list.append(targets)
+                # aug_seq_frag_list = [self.random_mutation(sequence,
+                #                                           [int(max(set(column))) for column in zip(*target)][:len(sequence)],
+                #                                           configs.train_settings.data_aug.mutation_rate)
+                #                      for sequence, target in zip(seq_frag_list, target_frag_list)]
                 aug_seq_frag_list = [self.random_mutation(sequence,
-                                                          [int(max(set(column))) for column in zip(*target)][:len(sequence)],
+                                                          target,
                                                           configs.train_settings.data_aug.mutation_rate)
-                                     for sequence, target in zip(seq_frag_list, target_frag_list)]
+                                     for sequence, target in zip(seq_frag_list, better_target_list)]
                 aug_target_frag_list = target_frag_list
                 aug_type_protein = type_protein
                 aug_samples.append((aug_id, aug_id_frag_list, aug_seq_frag_list, aug_target_frag_list, aug_type_protein))
