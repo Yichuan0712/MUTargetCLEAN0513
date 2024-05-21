34995e9037bcaa6e607715bfaabee0c35c23fec3
Yichuan <1758783038@qq.com>
2024-05-21 10:15:36 -0500

    Commit

--- run_hellbender.sh
+++ run_hellbender.sh
@@ -26,5 +26,5 @@ export HF_HOME=/home/yz3qt/data/transformers_cache/
 #--result_path ./results_supcon_hardTrue_onlysampling/b10_p2_n4/run2
 
 python train.py --config_path ./configs/config_nosupcon_CNNlinear_yichuan_0521.yaml \
---result_path ./result_nosupcon/0520aug
+--result_path ./result_nosupcon/0521aug
 #1594176
