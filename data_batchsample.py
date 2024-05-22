ee115ee2961cd015688199a1cd8a5086baaa2886
Yichuan <1758783038@qq.com>
2024-05-22 16:09:05 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -79,7 +79,7 @@ class LocalizationDataset(Dataset):
                 aug_id = id + "_" + str(aug_i)
                 aug_id_frag_list = [aug_id + "@" + id_frag.split("@")[1] for id_frag in id_frag_list]
                 aug_target_frag_list = target_frag_list.copy()
-                print(aug_target_frag_list)
+                print(len(aug_target_frag_list))
                 aug_seq_frag_list = [
                     self.random_mutation(sequence, [int(max(set(column))) for column in zip(*target)][:len(sequence)],
                                          configs.train_settings.data_aug.mutation_rate) for sequence, target in
