6bd798f260071166e3f8f7287b70849b26bd37bf
Yichuan <1758783038@qq.com>
2024-05-22 15:47:03 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -65,14 +65,11 @@ class LocalizationDataset(Dataset):
         print("data aug on len of "+str(len(samples)))
         aug_samples = []
         for id, id_frag_list, seq_frag_list, target_frag_list, type_protein in samples:
+            print(len(seq_frag_list))
             if configs.train_settings.data_aug.add_original:
                aug_samples.append((id, id_frag_list, seq_frag_list, target_frag_list, type_protein)) #add original
 
             class_positions = np.where(type_protein == 1)[0]
-            # print(len(target_frag_list[0]))
-            # exit(0)
-            # # print(type_protein)
-            #print(np.max([class_weights[x] for x in class_positions]))
             per_times = np.max([2, int(np.ceil(configs.train_settings.data_aug.per_times*np.max([class_weights[x] for x in class_positions])))])
             for aug_i in range(per_times):
                 aug_id = id+"_"+str(aug_i)
@@ -84,14 +81,14 @@ class LocalizationDataset(Dataset):
                 "Peroxisome" N/C
                 """
 
-                print(seq_frag_list)
-                print(target_frag_list)
+                # print(seq_frag_list)
+                # print(target_frag_list)
 
 
                 target_list = [[int(max(set(column))) for column in zip(*target)][:len(sequence)] for sequence, target
                                in zip(seq_frag_list, target_frag_list)]
-                print(target_list)
-                exit(0)
+                # print(target_list)
+                # exit(0)
 
                 for targets, ptype in zip(target_list, [class_positions[0]]*len(target_list)):
                     # print(ptype)
