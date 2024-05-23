fb2015fc3632ddf27290efa70dbb23e6dbae9bf4
Yichuan <1758783038@qq.com>
2024-05-22 21:10:04 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -89,6 +89,7 @@ class LocalizationDataset(Dataset):
                     if len(aug_target_frag_list) == 3:
                         flattened_aug_target_frag_list = [item for sublist in aug_target_frag_list for item in sublist]
                         print(len(flattened_aug_target_frag_list))
+                        exit(0)
                         if 1 in aug_target_frag_list[0][1]:
                             # print(len(aug_target_frag_list[0][1]))
                             # print(aug_target_frag_list[0][1])
