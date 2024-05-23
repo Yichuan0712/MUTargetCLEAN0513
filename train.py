3c8fe36f35d2236ea1fd9d1095c290d8e240bf3f
Yichuan <1758783038@qq.com>
2024-05-23 16:14:37 -0500

    Commit

--- train.py
+++ train.py
@@ -158,12 +158,32 @@ def train_loop(tools, configs, warm_starting,train_writer):
                 motif_logits, target_frag = loss_fix(id_frags_list, motif_logits, target_frag_pt, tools)
                 sample_weight_pt = torch.from_numpy(np.array(sample_weight_tuple)).to(tools['train_device']).unsqueeze(1)
                 print(motif_logits.shape)
-                print(target_frag.shape)
-                exit(0)
-                position_loss = tools['loss_function'](
-                                motif_logits, 
-                                target_frag.to(tools['train_device']))
-                
+                # print(target_frag.shape)
+                # exit(0)
+                if configs.train_settings.additional_pos_weights:
+
+                    motif_logits_nucleus = motif_logits[:, 0:1, :]
+                    print(motif_logits_nucleus.shape)
+                    motif_logits_nucleus_export = motif_logits[:, 4:5, :]
+                    print(motif_logits_nucleus_export.shape)
+                    # Concatenate the parts that exclude indices 0 and 4
+                    motif_logits = torch.cat((motif_logits[:, 1:4, :], motif_logits[:, 5:, :]), dim=1)
+                    print(motif_logits.shape)
+                    exit(0)
+
+                    target_frag_nucleus = target_frag[:, 0:1, :]
+                    target_frag_nucleus_export = target_frag[:, 4:5, :]
+                    # Concatenate the parts that exclude indices 0 and 4
+                    target_frag = torch.cat((target_frag[:, 1:4, :], target_frag[:, 5:, :]), dim=1)
+
+                    position_loss = tools['loss_function'](motif_logits, target_frag.to(tools['train_device']))
+                    nucleus_position_loss = tools['loss_function_nucleus']\
+                        (motif_logits_nucleus, target_frag_nucleus.to(tools['train_device']))
+                    nucleus_export_position_loss = tools['loss_function_nucleus_export']\
+                        (motif_logits_nucleus_export, target_frag_nucleus_export.to(tools['train_device']))
+
+                else:
+                    position_loss = tools['loss_function'](motif_logits, target_frag.to(tools['train_device']))
                 #class_weights = target_frag * (tools['pos_weight'] - 1) + 1 
                 #position_loss = torch.mean(position_loss * class_weights.to(tools['train_device']))
                 
@@ -171,11 +191,23 @@ def train_loop(tools, configs, warm_starting,train_writer):
                     class_loss  =  torch.mean(tools['loss_function_pro'](classification_head, type_protein_pt.to(tools['train_device']))) #remove sample_weight_pt
                 else:
                     class_loss  =  torch.mean(tools['loss_function_pro'](classification_head, type_protein_pt.to(tools['train_device'])) * sample_weight_pt)
-                
-                train_writer.add_scalar('step class_loss', class_loss.item(), global_step=global_step)
-                train_writer.add_scalar('step position_loss', position_loss.item(), global_step=global_step)
-                print(f"{global_step} class_loss:{class_loss.item()}  position_loss:{position_loss.item()}")
-                weighted_loss_sum=class_loss+position_loss
+
+                if configs.train_settings.additional_pos_weights:
+                    train_writer.add_scalar('step class_loss', class_loss.item(), global_step=global_step)
+                    train_writer.add_scalar('step position_loss', position_loss.item(), global_step=global_step)
+                    train_writer.add_scalar('step nucleus_position_loss', nucleus_position_loss.item(),
+                                            global_step=global_step)
+                    train_writer.add_scalar('step nucleus_export_position_loss', nucleus_export_position_loss.item(),
+                                            global_step=global_step)
+                    print(f"{global_step} class_loss:{class_loss.item()}  position_loss:{position_loss.item()}  " +
+                          f"nucleus_position_loss:{nucleus_position_loss.item()}  " +
+                          f"nucleus_export_position_loss:{nucleus_export_position_loss.item()}")
+                    weighted_loss_sum = class_loss + position_loss + nucleus_position_loss + nucleus_export_position_loss
+                else:
+                    train_writer.add_scalar('step class_loss', class_loss.item(), global_step=global_step)
+                    train_writer.add_scalar('step position_loss', position_loss.item(), global_step=global_step)
+                    print(f"{global_step} class_loss:{class_loss.item()}  position_loss:{position_loss.item()}")
+                    weighted_loss_sum = class_loss+position_loss
             
             if configs.supcon.apply and configs.supcon.apply_supcon_loss: #configs.supcon.apply: # and warm_starting: calculate supcon loss no matter whether warm_starting or not.
                 supcon_loss = tools['loss_function_supcon'](
@@ -579,8 +611,8 @@ def main(config_dict, args,valid_batch_number, test_batch_number):
 
     # w=(torch.ones([9,1,1])*5).to(configs.train_settings.device)
     w = torch.tensor(configs.train_settings.loss_pos_weight, dtype=torch.float32).to(configs.train_settings.device)
-    w_nucleus = torch.tensor(configs.train_settings.loss_pos_weight, dtype=torch.float32).to(configs.train_settings.device)
-    w_nucleus_export = torch.tensor(configs.train_settings.loss_pos_weight, dtype=torch.float32).to(configs.train_settings.device)
+    w_nucleus = torch.tensor(configs.train_settings.loss_pos_weight_nucleus, dtype=torch.float32).to(configs.train_settings.device)
+    w_nucleus_export = torch.tensor(configs.train_settings.loss_pos_weight_nucleus_export, dtype=torch.float32).to(configs.train_settings.device)
     #debug_dataloader(dataloaders_dict["train"]) #936 after call dataloaders_dict['train']
     
     tools = {
