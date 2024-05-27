bae234b60cf76beda22eea7e13ee8b109e46bd30
Yichuan <1758783038@qq.com>
2024-05-27 02:32:24 -0500

    Commit

--- data_batchsample.py
+++ data_batchsample.py
@@ -100,11 +100,11 @@ class LocalizationDataset(Dataset):
                         print(flattened_aug_target_frag_list[1].shape)
 
                         print(flattened_aug_target_frag_list[1])
-                        print(flattened_aug_target_frag_list[1][stop_left + 1:])
+                        print(flattened_aug_target_frag_list[1][stop_left:])
                         flattened_aug_target_frag_list[1][stop_left + 1:] = [1] * \
                                                                       (len(flattened_aug_target_frag_list[1]) - stop_left - 1)
                         print(flattened_aug_target_frag_list[1])
-                        print(flattened_aug_target_frag_list[1][stop_left + 1:])
+                        print(flattened_aug_target_frag_list[1][stop_left:])
                         exit(0)
 
                     if 1 in flattened_aug_target_frag_list[2]:
