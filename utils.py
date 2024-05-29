746a223e9da07aa2486399529f41c0e4904c78e6
Yichuan <1758783038@qq.com>
2024-05-28 21:26:34 -0500

    Commit

--- utils.py
+++ utils.py
@@ -76,7 +76,8 @@ def load_configs(config,args=None):
         
         if args.resume_path:
             tree_config.resume.resume_path = args.resume_path
-            print("yes")
+            print("!!! load")
+            print(tree_config.resume.resume_path)
             exit(0)
     
     #set configs value to default if doesn't have the attr
