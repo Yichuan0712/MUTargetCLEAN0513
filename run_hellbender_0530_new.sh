3ecc46a4cbf5e71f80e1975185571b351ae0d2e3
Yichuan <1758783038@qq.com>
2024-05-31 01:14:52 -0500

    Commit

--- run_hellbender_0530_new.sh
+++ run_hellbender_0530_new.sh
@@ -25,15 +25,15 @@ export HF_HOME=/home/yz3qt/data/transformers_cache/
 #--resume_path /cluster/pixstor/xudong-lab/duolin/MUTargetCLEAN/results_supcon_hardTrue_onlysampling/b10_p2_n4/2024-03-28__11-36-17/checkpoints//best_model.pth \
 #--result_path ./results_supcon_hardTrue_onlysampling/b10_p2_n4/run2
 
-python train.py --config_path ./configs/config0530yichuan.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_cnn.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_addorigin.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_droprate.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_gamma.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_pertime3.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_kernel5.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_kernel9.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_lossweight.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_noaug.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_posweight.yaml --result_path ./result_nosupcon/0530_new/
-python train.py --config_path ./configs/config0530yichuan_warmup.yaml --result_path ./result_nosupcon/0530_new/
+python train.py --config_path ./configs/config0530yichuan.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_cnn.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_addorigin.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_droprate.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_gamma.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_pertime3.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_kernel5.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_kernel9.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_lossweight.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_noaug.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_posweight.yaml --result_path ./result_nosupcon/0530_new/  || true
+python train.py --config_path ./configs/config0530yichuan_warmup.yaml --result_path ./result_nosupcon/0530_new/  || true
