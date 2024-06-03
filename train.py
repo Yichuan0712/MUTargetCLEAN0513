a33f690f8675ff835d13164a9581d822077a231e
Yichuan <1758783038@qq.com>
2024-06-03 16:02:27 -0500

    Commit

--- train.py
+++ train.py
@@ -385,7 +385,11 @@ def get_data_dict(args, dataloader, tools):
     # Unnecessary in this situation but added for best practices
     # model.eval().cuda()
     # model_path = os.path.join(tools['checkpoint_path'], f'best_model.pth')
-    model_path = args.resume_path
+    if args.predict == 1:
+       if os.path.exists(args.resume_path):
+           model_path = args.resume_path
+       else:
+          model_path = os.path.join(tools['checkpoint_path'], f'best_model.pth')
     model_checkpoint = torch.load(model_path, map_location='cpu')
     tools['net'].load_state_dict(model_checkpoint['model_state_dict'])
     tools['net'].eval().to(tools["valid_device"])
