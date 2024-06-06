d3139fd15778c01ede1246db708339bbb4c5fe59
Yichuan <1758783038@qq.com>
2024-06-06 12:47:45 -0500

    Commit

---
+++ hellbender_sh/run_hellbender_0606_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --mem 50G
+#SBATCH -n 1
+#SBATCH --gres gpu:A100:1
+
+#SBATCH --error=error.log           # 错误日志文件
+
+#SBATCH --time 2-00:00:00 #Time for the job to run
+#SBATCH --job-name mutarget
+#SBATCH -p gpu
+##SBATCH -p xudong-gpu
+##SBATCH -A xudong-lab
+
+
+module load miniconda3
+
+# Activate the Conda environment
+#source activate /home/wangdu/.conda/envs/pytorch1.13.0
+source activate /home/yz3qt/data/miniconda/envs/mutarget
+
+export TORCH_HOME=/home/yz3qt/data/torch_cache/
+export HF_HOME=/home/yz3qt/data/transformers_cache/
+
+python train.py --config_path ./configs_yaml/0606config/config0606yichuan_k5_max_dnn.yaml --result_path ./result_0606test/  || true
+python train.py --config_path ./configs_yaml/0606config/config0606yichuan_k7_max_dnn.yaml --result_path ./result_0606test/  || true
