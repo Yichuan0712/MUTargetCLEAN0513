2d4256a7ebfcc0c26e17fc812be0446a646a8978
Yichuan <1758783038@qq.com>
2024-06-20 11:50:16 -0500

    Commit

--- hellbender_sh/run_hellbender_0620.sh
+++ hellbender_sh/run_hellbender_0620.sh
@@ -23,6 +23,10 @@ source activate /home/yz3qt/data/miniconda/envs/mutarget
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python train.py --config_path ./configs_yaml/0619config/config0619_E15_T0.2_T5_addori.yaml --result_path ./result_0619/  || true
+#python train.py --config_path ./configs_yaml/0619config/config0619_E15_T0.2_T5_addori.yaml --result_path ./result_0619/  || true
 
+python train.py --config_path ./configs_yaml/0620config/config0620_E15_T0.2_T5_addori_lowlr1.yaml --result_path ./result_0620/  || true
+python train.py --config_path ./configs_yaml/0620config/config0620_E15_T0.2_T5_addori_lowlr1_no_export.yaml --result_path ./result_0620/  || true
 
+python train.py --config_path ./configs_yaml/0620config/config0620_E15_T0.2_T5_addori_lowlr2.yaml --result_path ./result_0620/  || true
+python train.py --config_path ./configs_yaml/0620config/config0620_E15_T0.2_T5_addori_lowlr2_no_export.yaml --result_path ./result_0620/  || true
