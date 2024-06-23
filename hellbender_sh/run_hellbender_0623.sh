1157721e0cd97b3e7330ad586b78cbc8286c02da
Yichuan <1758783038@qq.com>
2024-06-23 16:18:04 -0500

    Commit and Push

--- hellbender_sh/run_hellbender_0623.sh
+++ hellbender_sh/run_hellbender_0623.sh
@@ -23,6 +23,6 @@ source activate /home/yz3qt/data/miniconda/envs/mutarget
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python train.py --config_path ./configs_yaml/0621config/config0621_E100.yaml --result_path ./result_0623/  || true
+python train.py --config_path ./configs_yaml/0621config/config0621_E100.yaml --result_path ./result_0623/ --fold_num 0  || true
 
 
