0a0e51ceb2fbde35f6906fe1cbb924489c948c58
Yichuan <1758783038@qq.com>
2024-06-24 11:34:24 -0500

    Commit and Push

--- hellbender_sh/run_hellbender_0624_5fold.sh
+++ hellbender_sh/run_hellbender_0624_5fold.sh
@@ -23,7 +23,8 @@ source activate /home/yz3qt/data/miniconda/envs/mutarget
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python train.py --config_path ./configs_yaml/0622config/config0622_E15_M0.27_M0.09.yaml --result_path ./result_0622/ --fold_num 5 || true
+python train.py --config_path ./configs_yaml/0624config/config0624_E15_M0.3_M0.1.yaml --result_path ./result_0624_5fold1/ --fold_num 5 || true
 
+python train.py --config_path ./configs_yaml/0624config/config0624_E15_M0.5_M0.16.yaml --result_path ./result_0624_5fold2/ --fold_num 5 || true
 
 
