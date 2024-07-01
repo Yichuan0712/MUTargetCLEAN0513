ecc5f070d96ee121aad1fec49c5fb57f728781f5
Yichuan <1758783038@qq.com>
2024-07-01 14:18:01 -0500

    Commit and Push

--- hellbender_sh/run_hellbender_0701.sh
+++ hellbender_sh/run_hellbender_0701.sh
@@ -23,10 +23,6 @@ source activate /home/yz3qt/data/miniconda/envs/mutarget
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.3_M0.1.yaml --result_path ./result_0701/ || true
-
-python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.27_M0.09.yaml --result_path ./result_0701/ || true
-
 python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.3_M0.1_k5_addori.yaml --result_path ./result_0701/ || true
 
 python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.27_M0.09_k5_addori.yaml --result_path ./result_0701/ || true
@@ -36,3 +32,7 @@ python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.15_M0.
 python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.2_M0.067_addori.yaml --result_path ./result_0701/ || true
 
 python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.25_M0.083_addori.yaml --result_path ./result_0701/ || true
+
+python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.3_M0.1.yaml --result_path ./result_0701/ || true
+
+python train.py --config_path ./configs_yaml/0701config/config0701_E15_M0.27_M0.09.yaml --result_path ./result_0701/ || true
