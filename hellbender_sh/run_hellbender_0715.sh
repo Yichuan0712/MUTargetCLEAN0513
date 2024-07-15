431b5a4c7ea00cf7d1dc0a7abd23f0aa9c6e688a
Yichuan <1758783038@qq.com>
2024-07-15 14:45:17 -0500

    Commit and Push

---
+++ hellbender_sh/run_hellbender_0715.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mem 50G
+#SBATCH -n 1
+#SBATCH --gres gpu:A100:1
+
+#SBATCH --error=error.log           # 错误日志文件
+
+#SBATCH --time 4-00:00:00 #Time for the job to run
+#SBATCH --job-name mutarget
+
+#SBATCH -p xudong-gpu
+#SBATCH -A xudong-lab
+
+##SBATCH -p gpu
+
+
+module load miniconda3
+
+# Activate the Conda environment
+#source activate /home/wangdu/.conda/envs/pytorch1.13.0
+source activate /home/yz3qt/data/miniconda/envs/mutarget
+
+export TORCH_HOME=/home/yz3qt/data/torch_cache/
+export HF_HOME=/home/yz3qt/data/transformers_cache/
+
+python train.py --config_path ./configs_yaml/0715config_cdhit/config0715_E15_M0.3_M0.1_cdhit_signal.yaml --result_path ./result_0715/ --fold_num 5 || true
